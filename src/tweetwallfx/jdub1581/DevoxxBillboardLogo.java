173d2a4e8a4dee9df411b43c34d92cbc29c35f8a
Dub <Dub@Dub-PC>
2014-11-11 16:58:30 -0800

    Update to Devoxx Logo

--- src/tweetwallfx/jdub1581/DevoxxBillboardLogo.java
+++ src/tweetwallfx/jdub1581/DevoxxBillboardLogo.java
@@ -5,6 +5,7 @@
  */
 package tweetwallfx.jdub1581;
 
+import java.util.Arrays;
 import java.util.Random;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.FloatProperty;
@@ -26,37 +27,38 @@ import org.fxyz.extras.BillboardBehavior;
  *
  * @author Jason Pollastrini aka jdub1581
  */
-public class DevoxxBillboardLogo extends MeshView implements BillboardBehavior<DevoxxBillboardLogo>{
+public class DevoxxBillboardLogo extends MeshView implements BillboardBehavior<DevoxxBillboardLogo> {
 
     private final PhongMaterial mat = new PhongMaterial();
-    private final Image image = new Image(getClass().getResourceAsStream("logo.jpg"));    
+    private final Image image = new Image(getClass().getResourceAsStream("logo.jpg"));
     private final FXTimer updateThread = new FXTimer();
     private Camera otherNode;
+    private boolean smooth;
 
     public DevoxxBillboardLogo(double width, double height) {
-        this();        
-    }    
+        this();
+    }
 
     public DevoxxBillboardLogo() {
         this.mat.setDiffuseMap(image);
-
+        this.setSmoothing(smooth);
         this.mesh = createMesh();
         this.setMesh(mesh);
         this.setMaterial(mat);
         this.setCullFace(CullFace.NONE);
-        this.updateThread.setRunnable(()->{
+        this.updateThread.setRunnable(() -> {
             if (isAnimated() && updateThread.getCurrentTime() >= (updateThread.getUpdateDelay())) {
-                updateMesh();
+                computeMeshUpdate();
                 updateThread.setUpdateDelay((updateThread.getCurrentTime() + 80000000));
             }
         });
-        
+
         updateThread.start();
-        
+
         this.sceneProperty().addListener(new ChangeListener<Scene>() {
             @Override
             public void changed(ObservableValue<? extends Scene> observable, Scene oldValue, Scene newValue) {
-                if(newValue != null){
+                if (newValue != null) {
                     otherNode = getScene().getCamera();
                     startBillboardBehavior();
                     sceneProperty().removeListener(this);
@@ -87,8 +89,8 @@ public class DevoxxBillboardLogo extends MeshView implements BillboardBehavior<D
         return animated;
     }
     //==========================================================================
-    private final FloatProperty prefWidth = new SimpleFloatProperty(2000){
-        
+    private final FloatProperty prefWidth = new SimpleFloatProperty(2000) {
+
         @Override
         protected void invalidated() {
             updateProperties();
@@ -108,8 +110,8 @@ public class DevoxxBillboardLogo extends MeshView implements BillboardBehavior<D
         return prefWidth;
     }
     //==========================================================================
-    private final FloatProperty prefHeight = new SimpleFloatProperty(800){
-        
+    private final FloatProperty prefHeight = new SimpleFloatProperty(800) {
+
         @Override
         protected void invalidated() {
             updateProperties();
@@ -128,43 +130,50 @@ public class DevoxxBillboardLogo extends MeshView implements BillboardBehavior<D
     public FloatProperty prefHeightProperty() {
         return prefHeight;
     }
-    
+
+    //==========================================================================
+    public boolean isSmooth() {
+        return smooth;
+    }
+
+    public final void setSmoothing(boolean smooth) {
+        this.smooth = smooth;
+    }
+
 
     /*==========================================================================
      *          Create Mesh
      *///=======================================================================
-    
-    private final float defaultAmplitude = 75.0f;
-    private final float defaultFrequency = 20.0f, defaultPeriod = 60.5f, defaultWaveLength = 80.25f;
-    
-    private int divX = 64, divY = 64;
+    private final float defaultAmplitude = 30.0f;
+    private final float defaultFrequency = 2.75f, defaultPeriod = 0.5f, defaultWaveLength = 0.25f;
+
+    private int divs = 64;
     private int subX, subY;
 
     float[][][] pointCache;
 
-    private float holdPos, X;
+    private float holdPos;
 
     private TriangleMesh mesh;
-    
+
     private boolean needsUpdate;
-    
+
     private Random r = new Random();
-    
+
     //==========================================================================
     private TriangleMesh createMesh() {
 
         TriangleMesh m = newEmptyMesh();
 
-        float 
-                minX = -(float) getPrefWidth() / 2f,
-                maxX = (float)  getPrefWidth() / 2f,
+        float minX = -(float) getPrefWidth() / 2f,
+                maxX = (float) getPrefWidth() / 2f,
                 minY = -(float) getPrefHeight() / 2f,
-                maxY =  (float) getPrefHeight() / 2f;
+                maxY = (float) getPrefHeight() / 2f;
 
-        subX = (int) getPrefWidth() / divX;
-        subY = (int) getPrefHeight()/ divY;
+        subX = (int) getPrefWidth() / divs;
+        subY = (int) getPrefHeight() / divs;
         if (pointCache == null) {
-            pointCache = new float[divX][divY][3];
+            pointCache = new float[divs * subX][divs * subY][3];
         }
         final int pointSize = m.getPointElementSize();
         final int texCoordSize = m.getTexCoordElementSize();
@@ -177,111 +186,36 @@ public class DevoxxBillboardLogo extends MeshView implements BillboardBehavior<D
         float texCoords[] = new float[numVerts * texCoordSize];
         int faceCount = subX * subY * 2;
         int faces[] = new int[faceCount * faceSize];
+        int[] smoothing = new int[faceCount];
 
-        /*======================================================================
-            if animated and pointCache needs update
-        */
-        if(isAnimated() && needsUpdate){
-            for (int y = 0; y <= subY; y++) {
+        // Create pointPos and texCoords
+        for (int y = 0; y <= subY; y++) {
 
-                float currY = (float) y / subY;
-                double fy = (1 - currY) * minY + currY * maxY;
+            float currY = (float) y / subY;
+            double fy = (1 - currY) * minY + currY * maxY;
 
-                for (int x = 0; x <= subX; x++) {
+            for (int x = 0; x <= subX; x++) {
 
-                    float currX = (float) x / subX;
-                    double fx = (1 - currX) * minX + currX * maxX;
-                    int index = y * numDivX * pointSize + (x * pointSize);
-                    // Apply The Wave To Our Mesh
-                                       
-                    if(r.nextBoolean()){
-                        pointCache[x][y][0] = (float) (fx);
-                        pointCache[x][y][1] = (float) (fy);
-                        pointCache[x][y][2] = (float) (getAmplitude() * (Math.sin(((waveNumber.get()) * x) - (getFrequency() * currX))) + getAmplitude() * r.nextFloat());
-                    }else{
-                        pointCache[x][y][0] = (float) (fx);
-                        pointCache[x][y][1] = (float) (fy);
-                        pointCache[x][y][2] = (float) (getAmplitude() * (Math.sin(((waveNumber.get()) * x) - (getFrequency() * currX))));
-                    }
+                float currX = (float) x / subX;
+                double fx = (1 - currX) * minX + currX * maxX;
+                int index = y * numDivX * pointSize + (x * pointSize);
 
-                    points[index + 0] = pointCache[x][y][0];
-                    points[index + 1] = pointCache[x][y][1];
-                    points[index + 2] = pointCache[x][y][2];
+                // Apply The Wave To Our Mesh on XZ plane                     
+                pointCache[x][y][0] = (float) (fx);
+                pointCache[x][y][1] = (float) (fy);
+                pointCache[x][y][2] = (float) (getAmplitude() * (Math.sin(((waveNumber.get()) * x) - (getFrequency() * currX))));
 
-                    index = y * numDivX * texCoordSize + (x * texCoordSize);
-                    texCoords[index] = currX;
-                    texCoords[index + 1] = currY;
+                points[index + 0] = pointCache[x][y][0] + r.nextFloat();
+                points[index + 1] = pointCache[x][y][1] + r.nextFloat();
+                points[index + 2] = pointCache[x][y][2];
 
-                }
-            }
-            needsUpdate = false;
-        } 
-        /*======================================================================
-            step values if animated        
-        */
-        else if (isAnimated() && !needsUpdate) {
-            // update pointPos and texCoords
-            int index = 0;
-            int texIndex;
-            float currX = 0;
-            for (int y = 0; y <= subY; y++) {
-                float currY = (float) y / subY;
-                // Store Current X Value, Left Side Of Wave
-                holdPos = pointCache[0][y][2];
+                index = y * numDivX * texCoordSize + (x * texCoordSize);
+                texCoords[index] = currX;
+                texCoords[index + 1] = currY;
 
-                for (int x = 0; x <= subX; x++) {
-                    currX = (float) x / subX;
-                    index = y * (subX + 1) * 3 + (x * 3);
-                    // Current Wave Value Equals Value To The Right
-                    pointCache[x][y][2] = pointCache[x + 1][y][2];
-
-                    //setPoints from cache
-                    points[index + 0] = pointCache[x][y][0];
-                    points[index + 1] = pointCache[x][y][1];
-                    points[index + 2] = pointCache[x][y][2];
-
-                    texIndex = y * numDivX * texCoordSize + (x * texCoordSize);
-                    texCoords[texIndex] = currX;
-                    texCoords[texIndex + 1] = currY;
-
-                }
-                // Last Value Becomes The Far Left Stored Value
-                pointCache[subX][y][2] = holdPos;
-            }
-        } 
-        /*======================================================================
-            else create default points  
-        */
-        else {
-            // Create pointPos and texCoords
-            for (int y = 0; y <= subY; y++) {
-
-                float currY = (float) y / subY;
-                double fy = (1 - currY) * minY + currY * maxY;
-
-                for (int x = 0; x <= subX; x++) {
-
-                    float currX = (float) x / subX;
-                    double fx = (1 - currX) * minX + currX * maxX;
-                    int index = y * numDivX * pointSize + (x * pointSize);
-                    
-                    // Apply The Wave To Our Mesh on XZ plane                     
-                    
-                    pointCache[x][y][0] = (float) (fx);
-                    pointCache[x][y][1] = (float) (fy);
-                    pointCache[x][y][2] = (float) (getAmplitude() * (Math.sin(((waveNumber.get()) * x) - (getFrequency() * currX))));
-
-                    points[index + 0] = pointCache[x][y][0];
-                    points[index + 1] = pointCache[x][y][1];
-                    points[index + 2] = pointCache[x][y][2];
-
-                    index = y * numDivX * texCoordSize + (x * texCoordSize);
-                    texCoords[index] = currX;
-                    texCoords[index + 1] = currY;
-
-                }
             }
         }
+
         // Create faces
         for (int y = 0; y < subY; y++) {
             for (int x = 0; x < subX; x++) {
@@ -311,29 +245,104 @@ public class DevoxxBillboardLogo extends MeshView implements BillboardBehavior<D
                 faces[index + 5] = tc00;
             }
         }
+        if (smooth == true) {
+            Arrays.fill(smoothing, 1);
+        } else {
+            for (int i = 0; i < smoothing.length; i++) {
+                smoothing[i] = i % 32;
+                // System.err.println("XXX smoothGroups[" + i + "] = " + smoothGroups[i]);
+            }
+        }
         //updatePoints();
         m.getPoints().addAll(points);
         m.getTexCoords().addAll(texCoords);
         m.getFaces().addAll(faces);
         return m;
     }
+
     //==========================================================================
     private TriangleMesh newEmptyMesh() {
         return new TriangleMesh();
     }
 
-    private void updateMesh() {        
-        mesh = createMesh();
-        setMesh(mesh);
+    private void computeMeshUpdate() {
+        float[] points = mesh.getPoints().toArray(null);
+
+        float minX = -(float) getPrefWidth() / 2f,
+                maxX = (float) getPrefWidth() / 2f,
+                minY = -(float) getPrefHeight() / 2f,
+                maxY = (float) getPrefHeight() / 2f;
+        int numDivX = subX + 1;
+
+        if (isAnimated() && needsUpdate) {
+            for (int y = 0; y <= subY; y++) {
+
+                float currY = (float) y / subY;
+                double fy = (1 - currY) * minY + currY * maxY;
+
+                for (int x = 0; x <= subX; x++) {
+
+                    float currX = (float) x / subX;
+                    double fx = (1 - currX) * minX + currX * maxX;
+                    int index = y * numDivX * mesh.getPointElementSize() + (x * mesh.getPointElementSize());
+                    // Apply The Wave To Our Mesh
+
+                    if (r.nextBoolean()) {
+                        pointCache[x][y][0] = (float) (fx);
+                        pointCache[x][y][1] = (float) (fy);
+                        pointCache[x][y][2] = (float) (getAmplitude() * (Math.sin(((waveNumber.get()) * x) - (getFrequency() * currX))) + (getFrequency()));
+                    } else {
+                        pointCache[x][y][0] = (float) (fx);
+                        pointCache[x][y][1] = (float) (fy);
+                        pointCache[x][y][2] = (float) (getAmplitude() * (Math.sin(((waveNumber.get()) * x) - (getFrequency() * currX))));
+                    }
+
+                    points[index + 0] = pointCache[x][y][0] + r.nextFloat();
+                    points[index + 1] = pointCache[x][y][1] + r.nextFloat();
+                    points[index + 2] = pointCache[x][y][2];
+                }
+            }
+            needsUpdate = false;
+        } /*======================================================================
+         step values if animated        
+         */ else if (isAnimated() && !needsUpdate) {
+            // update pointPos and texCoords
+            int index = 0;
+            int texIndex;
+            float currX = 0;
+            for (int y = 0; y <= subY; y++) {
+                float currY = (float) y / subY;
+                // Store Current X Value, Left Side Of Wave
+                holdPos = pointCache[0][y][2];
+
+                for (int x = 0; x <= subX; x++) {
+
+                    currX = (float) x / subX;
+                    index = y * (subX + 1) * 3 + (x * 3);
+                    // Current Wave Value Equals Value To The Right
+                    pointCache[x][y][2] = pointCache[x + 1][y][2];
+                    //setPoints from cache
+                    points[index + 0] = pointCache[x][y][0];
+                    points[index + 1] = pointCache[x][y][1];
+                    points[index + 2] = pointCache[x][y][2];
+
+                }
+                // Last Value Becomes The Far Left Stored Value
+                pointCache[subX][y][2] = holdPos;
+            }
+        }
+
+        mesh.getPoints().set(0, points, 0, points.length);
     }
+
     //==========================================================================
-    private final FloatProperty pi2 = new SimpleFloatProperty((float)Math.PI * 2);
+    private final FloatProperty pi2 = new SimpleFloatProperty((float) Math.PI * 2);
     //==========================================================================
     private final FloatProperty frequency = new SimpleFloatProperty(defaultFrequency) {
-        
+
         @Override
         protected void invalidated() {
-            updateProperties();
+            //updateProperties();
             needsUpdate = true;
         }
     };
@@ -353,7 +362,7 @@ public class DevoxxBillboardLogo extends MeshView implements BillboardBehavior<D
     private final FloatProperty period = new SimpleFloatProperty(defaultPeriod) {
         @Override
         protected void invalidated() {
-            updateProperties();
+            //updateProperties();
             needsUpdate = true;
         }
     };
@@ -373,7 +382,7 @@ public class DevoxxBillboardLogo extends MeshView implements BillboardBehavior<D
     private final FloatProperty waveLength = new SimpleFloatProperty(defaultWaveLength) {
         @Override
         protected void invalidated() {
-            updateProperties();
+            //updateProperties();
             needsUpdate = true;
         }
     };
@@ -389,12 +398,12 @@ public class DevoxxBillboardLogo extends MeshView implements BillboardBehavior<D
     public FloatProperty waveLengthProperty() {
         return waveLength;
     }
-    
+
     //==========================================================================
     private final FloatProperty amplitude = new SimpleFloatProperty(defaultAmplitude) {
         @Override
         protected void invalidated() {
-            updateProperties();
+            //updateProperties();
             needsUpdate = true;
         }
     };
@@ -410,18 +419,18 @@ public class DevoxxBillboardLogo extends MeshView implements BillboardBehavior<D
     public FloatProperty amplitudeProperty() {
         return amplitude;
     }
-    
+
     //==========================================================================
     private final FloatProperty waveSpeed = new SimpleFloatProperty();
     //==========================================================================
     private final FloatProperty waveNumber = new SimpleFloatProperty();
-    
+
     //==========================================================================
-    private void updateProperties(){
+    private void updateProperties() {
         waveSpeed.set(waveLength.divide(period).floatValue());
-        waveNumber.set((pi2.divide(period)).floatValue());        
+        waveNumber.set((pi2.divide(period)).floatValue());
     }
-    
+
     public final void start() {
         updateThread.start();
     }
@@ -441,5 +450,4 @@ public class DevoxxBillboardLogo extends MeshView implements BillboardBehavior<D
         return otherNode;
     }
 
-    
 }
